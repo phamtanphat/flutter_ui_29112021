71b5e90100735d2ef8ffa6de5860abd09e672418
phamtanphat <phatdroid94@gmail.com>
2021-12-17 21:00:07 +0700

    textfield

--- lib/main.dart
+++ lib/main.dart
@@ -19,92 +19,24 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page"),
+        title: Text("Calculator"),
       ),
       body: SafeArea(
           child: Container(
+            padding: EdgeInsets.all(5),
             constraints: BoxConstraints.expand(),
-            color: Colors.teal,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  height: MediaQuery.of(context).size.height / 2.5,
-                  width: double.infinity,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                      top: 0,
-                      left: 0,
-                      child: Container(
-                        alignment: Alignment.center,
-                        color: Colors.red,
-                        width: MediaQuery.of(context).size.width / 4.5,
-                        height: MediaQuery.of(context).size.width / 10,
-                        child: Text("1"),
-                      )),
-                      Positioned(
-                          top: MediaQuery.of(context).size.width / 10,
-                          left: MediaQuery.of(context).size.width / 4.5,
-                          child: Container(
-                            alignment: Alignment.center,
-                            color: Colors.red,
-                            width: MediaQuery.of(context).size.width / 4.5,
-                            height: MediaQuery.of(context).size.width / 10,
-                            child: Text("1"),
-                          )),
-                      Positioned(
-                          top: (MediaQuery.of(context).size.width / 10 * 2),
-                          left: (MediaQuery.of(context).size.width / 4.5) * 2,
-                          child: Container(
-                            alignment: Alignment.center,
-                            color: Colors.red,
-                            width: MediaQuery.of(context).size.width / 4.5,
-                            height: MediaQuery.of(context).size.width / 10,
-                            child: Text("1"),
-                          )),
-                      Positioned(
-                          top: (MediaQuery.of(context).size.width / 10 * 3),
-                          left: (MediaQuery.of(context).size.width / 4.5) * 3,
-                          child: Container(
-                            alignment: Alignment.center,
-                            color: Colors.red,
-                            width: MediaQuery.of(context).size.width / 4.5,
-                            height: MediaQuery.of(context).size.width / 10,
-                            child: Text("1"),
-                          )),
-                      Positioned(
-                          top: (MediaQuery.of(context).size.width / 10 * 4),
-                          left: (MediaQuery.of(context).size.width / 4.5) * 2,
-                          child: Container(
-                            alignment: Alignment.center,
-                            color: Colors.red,
-                            width: MediaQuery.of(context).size.width / 4.5,
-                            height: MediaQuery.of(context).size.width / 10,
-                            child: Text("1"),
-                          )),
-                      Positioned(
-                          top:(MediaQuery.of(context).size.width / 10 * 5),
-                          left: MediaQuery.of(context).size.width / 4.5,
-                          child: Container(
-                            alignment: Alignment.center,
-                            color: Colors.red,
-                            width: MediaQuery.of(context).size.width / 4.5,
-                            height: MediaQuery.of(context).size.width / 10,
-                            child: Text("1"),
-                          )),
-                      Positioned(
-                          top: (MediaQuery.of(context).size.width / 10 * 6),
-                          left: 0,
-                          child: Container(
-                            alignment: Alignment.center,
-                            color: Colors.red,
-                            width: MediaQuery.of(context).size.width / 4.5,
-                            height: MediaQuery.of(context).size.width / 10,
-                            child: Text("1"),
-                          )),
-                    ],
+                TextField(
+                  obscureText: false,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.email),
+                    hintText: "Input Number 1",
+                    // disabledBorder: InputBorder.none,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5))
+                    )
                   ),
                 )
               ],
